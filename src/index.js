0a9ee97356d9485ec6eee3693aa2d4a1629b61f3
leewooseong <sitpo96@naver.com>
2023-02-06 00:00:30 +0900

    feat : TodoApp template 및 jsdoc 작성, docs 삭제
    - TodoList 및 TodoItem에 해당하는 template을 class를 활용하여 작성(내부 구현은 하지 않음)
    - template 내 사용되는 클래스 함수 및 변수에 대해 jsdoc으로 문서화
    - 이전 초기셋팅 때 jsdoc 실행 결과물로 생겼던 docs 디렉터리 삭제

--- src/index.js
+++ src/index.js
@@ -1,51 +1,135 @@
 /**
- * Represents a book.
- * @constructor
- * @param {string} title - The title of the book.
- * @param {string} author - The author of the book.
+ * tag Data
+ * @typedef {Object} TagData
+ * @property {number} tagId - id of tag
+ * @property {string} tagName - name of tag
  */
-function Book(title, author) {}
+/**
+ * Class representing a TodoItem.
+ * @property {string} userId - The id of User.
+ * @property {string} content - The description of todoItem.
+ * @property {string} category - The category of todoItem.
+ * @property {TagData[]} [tags=[]] - The tagInfo List of todoItem, this is optional parameter
+ */
+class TodoItem {
+    // CREATE Methods ...
+    /**
+     * Create a TodoItem DomElement
+     * @param {string} userId - The id of User.
+     * @param {string} content - The description of todoItem.
+     * @param {string} category - The category of todoItem.
+     * @param {TagData[]} [tags=[]] - The tagInfo List of todoItem, this is optional parameter
+     */
+    constructor(userId, content, category, tags) {
+        //...
+    }
+
+    // UPDATE Methods...
+    /**
+     * update content
+     * @param {string} content - parmaDescription
+     */
+    updateContent(content) {
+        // ...
+    }
+    /**
+     * update category
+     * @param {string} category - parmaDescription
+     */
+    updateCategory(category) {
+        // ...
+    }
+    /**
+     * update tagInfo
+     * @param {number} tagId - parmaDescription
+     * @param {string} tagName - parmaDescription
+     */
+    updateTag(tagId, tagName) {
+        // ...
+    }
+
+    // DELETE Methods...
+    /**
+     * delete todoItem's tag
+     * @param {Symbol} tagId - delete tag which has this tagId
+     */
+    deleteTag(tagId) {
+        //...
+    }
+    /**
+     * delete todoItem's tags all
+     */
+    deleteTagAll() {
+        //...
+    }
+
+    // READ Methods...
+    /**
+     * render todoItem UI
+     * @param {HTMLElement} parentNode - todoItem UI(HTMLElement) will be append to parentNode
+     */
+    render(parentNode) {
+        //...
+    }
+}
 
 /**
- * Both of these will link to the bar function.
- * @see {@link bar}
- * @see bar
+ * todoList Data
+ * @typedef {Object} TodoItemData
+ * @property {number} id - id of todoItem
+ * @property {TodoItem} todoItem - instance of todoItem class
  */
-function foo() {}
-
-// Use the inline {@link} tag to include a link within a free-form description.
 /**
- * @see {@link foo} for further information.
- * @see {@link http://github.com|GitHub}
+ * Class representing a TodoList.
+ * @property {TodoItemData[]} todoListData - data list
  */
-function bar() {}
+class TodoList {
+    constructor() {}
 
-/**
- * Generic dairy product.
- * @constructor
- */
-function DairyProduct() {}
+    // CREATE Methods...
+    /**
+     * create TodoItem Element using TodoItem class and add to todoListData with random todoItemId
+     * @param {string} userId - The id of todoItem.
+     * @param {string} content - The description of todoItem.
+     * @param {string} category - The category of todoItem.
+     * @param {TagData[]} [tags=[]] - The tagInfo List of todoItem, this is optional parameter
+     */
+    addTodoItem(userId, content, category, tags) {
+        //...
+    }
 
-/**
- * Check whether the dairy product is solid at room temperature.
- * @abstract
- * @return {boolean}
- */
-DairyProduct.prototype.isSolid = function () {
-	throw new Error('must be implemented by subclass!');
-};
+    // DELETE Methods...
+    /**
+     * delete specific user's TodoItem by itemId
+     * @param {string} userId - delete todoItem which has this userId
+     * @param {number} todoId - delete todoItem which has this todoId
+     */
+    deleteTodoItem(userId, todoId) {
+        // ...
+    }
+    /**
+     * delete specific user's TodoList
+     * @param {string} userId - delete todoList which has this userId
+     */
+    deleteTodoList(userId) {
+        //...
+    }
 
-/**
- * Cool, refreshing milk.
- * @constructor
- * @augments DairyProduct
- */
-function Milk() {}
-
-/**
- * Check whether milk is solid at room temperature.
- * @return {boolean} Always returns false.
- */
-Milk.prototype.isSolid = function () {
-	return false;
-};
+    // READ Methods...
+    /**
+     * filter TodoItemList by userId
+     * this function will be used when the page is rendered with filtered data
+     * @param {string} userId - userId will be used for Filtering TodoItemList
+     * @return {TodoItemData[]} - return filtered todoList
+     */
+    filterTodoListByUser(userId) {
+        // ...
+    }
+    /**
+     * rendering Page with TodoItemList
+     * TodoItemList will be filtered by userId When user want to render user's todoList
+     */
+    render() {
+        // ...
+    }
+}
